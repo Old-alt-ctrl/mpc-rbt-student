62fbb618c0d46d042531e5ed129f4cdf20ec438b
Oliver Drga <247362@vutbr.cz>
2026-03-03 18:08:54 +0100

    almost

--- run_receiver.sh
+++ run_receiver.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ./build/receiver_node config.json
+source /opt/ros/humble/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/oliver/workspace/mpc-rbt-student/build
--- run_sender.sh
+++ run_sender.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+LOG_LEVEL=2
+source /opt/ros/humble/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/oliver/workspace/mpc-rbt-student/build
 ./build/sender_node config.json
